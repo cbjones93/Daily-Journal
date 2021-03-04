489b43c2e6e13f0042cc2cf52f3c94e4b00fbe8f
Cody_Jones <cbjones93@gmail.com>
2021-03-04 11:56:53 -0600

    javascript entries now working in html

---
+++ scripts/JournalData.js
@@ -0,0 +1,30 @@
+/*
+ *   Journal data for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    },
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const getJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
 console.log("Welcome to the main module")
+import { EntryListComponent } from "./JournalEntryList.js"
+EntryListComponent();
