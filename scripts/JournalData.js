e9b09cba8b5ba775b2914415ff8a7a0c5b9a83f8
Cody_Jones <cbjones93@gmail.com>
2021-03-07 16:53:09 -0600

    added json to daily journal

--- scripts/JournalData.js
+++ scripts/JournalData.js
@@ -7,24 +7,21 @@
  */
 
 // This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "07/24/2025",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok"
-    },
-]
+// const journal = [
+//     {
+//         id: 1,
+//         date: "07/24/2025",
+//         concept: "HTML & CSS",
+//         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+//         mood: "Ok"
+//     },
+// ]
 
 /*
     You export a function that provides a version of the
     raw data in the format that you want
 */
 export const getJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+return fetch ("http://localhost:8088/journalEntries")
+.then (response =>response.json())
 }
