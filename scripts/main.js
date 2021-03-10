9ddf596abcf51f6eea80d5b831b995630894e3bf
Cody_Jones <cbjones93@gmail.com>
2021-03-10 15:00:29 -0600

    filters not working

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 console.log("Welcome to the main module")
 import { journalList } from "../scripts/feed/JournalEntryList.js"
-import { getJournalEntries } from "../scripts/Data/DataManager.js"
+import { getJournalEntries,usePostCollection } from "../scripts/Data/DataManager.js"
 // import { EntryListComponent } from "./JournalEntryList.js"
 // EntryListComponent();
 const showJournalList = ()=>{
@@ -18,23 +18,40 @@ applicationElement.addEventListener('click', event => {
         alert(`Entry Recorded!`)
     }
 })
-applicationElement.addEventListener("click", event => {
-	if (event.target.id === "date"){
-		console.log("user clicked on date")
-	}
-})
-applicationElement.addEventListener("click", event => {
-	if (event.target.id === "conceptsCovered"){
-		console.log("user clicked on concepts covered")
-	}
-})
-applicationElement.addEventListener("click", event => {
-	if (event.target.id === "journalEntry"){
-		console.log("user clicked on journal entry")
-	}
-})
-applicationElement.addEventListener("click", event => {
+// applicationElement.addEventListener("change", event => {
+// 	if (event.target.id === "date"){
+// 		const dateAsNumber = parseInt(event.target.value)
+// 		console.log(`User wants to see posts since ${dateAsNumber}`)
+// 		showFilteredDatePosts(dateAsNumber);
+// 	}
+// })
+
+applicationElement.addEventListener("change", event => {
 	if (event.target.id === "mood"){
-		console.log("user clicked on mood")
+		const moodValue = (event.target.value)
+		console.log(`User wants to see posts with a mood of ${moodValue}`)
+		showFilteredMoodPosts(moodValue);
 	}
 })
+// const showFilteredDatePosts = (date) => {
+// 	//get a copy of the post collection
+// 	const epoch = Date.parse(`${date}`);
+// 	//filter the data
+// 	const filteredData = usePostCollection().filter(singlePost => {
+// 	  if (singlePost.timestamp >= epoch) {
+// 		return singlePost
+// 	  }
+// 	})
+// 	const postElement = document.querySelector(".journalEntryBox");
+// 	postElement.innerHTML = journalList(filteredData);
+//   }
+
+const showFilteredMoodPosts = () =>{
+	const filteredMood = usePostCollection().filter(singePost=>{
+		if(singlePost.mood === "Okay"){
+			return singePost
+		}
+	})
+	const postMoodElement = document.querySelector(".journalEntryBox");
+	postMoodElement.innerHTML = journalList(filteredMoodDate);
+}
